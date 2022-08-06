5298aa1d080bed3d8e7600fbe195c46cb903c818
Rafael-Lacorte <rafaellacorte14@gmail.com>
2022-08-06 13:32:12 -0300

    exercícios do dia

---
+++ front-end/bloco-09-javascript-e-testes-assincronos/dia-01-javascript-assincrono-e-callbacks/exercise04.js
@@ -0,0 +1,13 @@
+const messageDelay = () => Math.floor(Math.random() * 5000);
+
+const getMarsTemperature = () => {
+  const maxTemperature = 58;
+  return Math.floor(Math.random() * maxTemperature)
+};
+
+const sendMarsTemperature = () => {
+  const currentTemperature = getMarsTemperature();
+  setTimeout(() => console.log(`A temperatura de Marte é: ${currentTemperature} graus celsius`), messageDelay())
+};
+
+sendMarsTemperature()
