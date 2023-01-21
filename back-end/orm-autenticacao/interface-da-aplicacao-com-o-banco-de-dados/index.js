290bf89f9835145c5fa67e4885a08b968ff89789
Rafael-Lacorte <rafaellacorte14@gmail.com>
2023-01-21 16:31:11 -0300

    exercicios do dia

---
+++ back-end/orm-autenticacao/interface-da-aplicacao-com-o-banco-de-dados/index.js
@@ -0,0 +1,19 @@
+const express = require('express');
+const BooksController = require('./controllers/book.controller')
+const app = express();
+const PORT = process.env.PORT || 3009;
+
+app.use(express.json());
+
+app.get('/books', BooksController.getAll);
+
+app.get('/books/:id', BooksController.getById);
+
+app.post('/books', BooksController.create);
+
+app.put('/books/:id', BooksController.update);
+
+app.delete('/books/:id', BooksController.remove);
+
+
+app.listen(PORT, () => console.log(`Ouvindo na porta ${PORT}!`));
