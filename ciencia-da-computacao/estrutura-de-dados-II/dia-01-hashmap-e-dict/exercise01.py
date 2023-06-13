87c5823ec2d01ad1e19e3fd6e290f8d1fee74c70
Rafael-Lacorte <rafaellacorte14@gmail.com>
2023-06-13 17:28:13 -0300

    exercicio 01

--- ciencia-da-computacao/estrutura-de-dados-II/dia-01-hashmap-e-dict/exercise01.py
+++ ciencia-da-computacao/estrutura-de-dados-II/dia-01-hashmap-e-dict/exercise01.py
@@ -2,14 +2,7 @@ class Score:
 
   def __init__(self):
     self.scores = {}
-
-  def calculate_score(self, person):
-    if person in self.scores:
-      print('Já calculei palhaço, receba')
-      return self.scores[person]
-  
-    score = 1
-    subordinates = {
+    self.subordinates = {
       1: [2, 3],
       2: [4],
       3: [],
@@ -18,16 +11,36 @@ class Score:
       6: [],
       7: [],
     }
+  
+  def add_subordinate(self, person):
+    k = 5
+    new_person = len(self.subordinates) + 1
+    self.subordinates[new_person] = []
 
-    for p in subordinates[person]:
+    if (len(self.subordinates[person]) >= k):
+      return self.add_subordinate(person + 1)
+    self.subordinates[person].append(new_person)
+
+    return self.subordinates
+
+  def calculate_score(self, person):
+    if person in self.scores:
+      print('Já calculei palhaço, receba')
+      return self.scores[person]
+  
+    score = 1
+
+    for p in self.subordinates[person]:
       score += self.calculate_score(p)
     
     self.scores[person] = score
 
     return score
 
+  
+
 score_board = Score()
 
 print(score_board.calculate_score(1))
 
-print(score_board.calculate_score(1))
+print(score_board.add_subordinate(1))
