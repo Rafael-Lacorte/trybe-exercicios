841b44b588957fa3d42ab8d0ee0f53928ab820eb
Rafael-Lacorte <rafaellacorte14@gmail.com>
2023-06-13 17:14:48 -0300

    exercicio 01

--- ciencia-da-computacao/estrutura-de-dados-II/dia-01-hashmap-e-dict/exercise01.py
+++ ciencia-da-computacao/estrutura-de-dados-II/dia-01-hashmap-e-dict/exercise01.py
@@ -1,18 +1,33 @@
-def calculate_score(person):
-  score = 1
-  subordinates = {
-    1: [2, 3],
-    2: [4],
-    3: [],
-    4: [5, 6],
-    5: [7],
-    6: [],
-    7: [],
-  }
+class Score:
 
-  for p in subordinates[person]:
-    score += calculate_score(p)
+  def __init__(self):
+    self.scores = {}
+
+  def calculate_score(self, person):
+    if person in self.scores:
+      print('Já calculei palhaço, receba')
+      return self.scores[person]
   
-  return score
+    score = 1
+    subordinates = {
+      1: [2, 3],
+      2: [4],
+      3: [],
+      4: [5, 6],
+      5: [7],
+      6: [],
+      7: [],
+    }
 
-print(calculate_score(5))
+    for p in subordinates[person]:
+      score += self.calculate_score(p)
+    
+    self.scores[person] = score
+
+    return score
+
+score_board = Score()
+
+print(score_board.calculate_score(1))
+
+print(score_board.calculate_score(1))
